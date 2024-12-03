3febfbfb2355f183738747aa55c9ccad8cd7bdb1
Your Name <you@example.com>
2024-12-02 22:32:00 -0300

    OI

--- backend/src/main/java/org/success/payment/stripe/StripeContext.java
+++ backend/src/main/java/org/success/payment/stripe/StripeContext.java
@@ -9,22 +9,55 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 public abstract class StripeContext {
-    @Value("${stripe-public-key}")
+
     protected String publicKey;
 
-    @Value("${stripe-private-key}")
     protected String privateKey;
 
-    @Value("${stripe-lookup-key}")
     protected String lookupKey;
+
+    @Value("${app.on-production}")
+    private Boolean onProduction;
+
+    @Value("${stripe.dev.public-key}")
+    private String devPublicKey;
+
+    @Value("${stripe.dev.private-key}")
+    private String devPrivateKey;
+
+    @Value("${stripe.dev.lookup-key}")
+    private String devLookupKey;
+
+    @Value("${stripe.prod.public-key}")
+    private String prodPublicKey;
+
+    @Value("${stripe.prod.private-key}")
+    private String prodPrivateKey;
+
+    @Value("${stripe.prod.lookup-key}")
+    private String prodLookupKey;
+
 
     @PostConstruct
     public void setLookupKey() throws StripeException {
+        getContext();
         System.out.println(this.lookupKey);
         Stripe.apiKey = this.privateKey;
         Price resource = Price.retrieve("price_1QRIRfG3TGHQ3eAX84Zcz8Qu");
         PriceUpdateParams params =
                 PriceUpdateParams.builder().setLookupKey(this.lookupKey).build();
         Price price = resource.update(params);
+    }
+
+    private void getContext(){
+        if(onProduction){
+            this.publicKey = this.prodPublicKey;
+            this.privateKey = this.prodPrivateKey;
+            this.lookupKey = this.prodLookupKey;
+        }else{
+            this.publicKey = this.devPublicKey;
+            this.privateKey = this.devPrivateKey;
+            this.lookupKey = this.devLookupKey;
+        }
     }
 }
