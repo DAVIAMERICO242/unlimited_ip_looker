7e1f49f57af9dbe60483ee78c8ed8c5a1cc7deb6
Your Name <you@example.com>
2024-12-03 00:24:44 -0300

    OI

--- backend/src/main/java/org/success/payment/stripe/controllers/StripeController.java
+++ backend/src/main/java/org/success/payment/stripe/controllers/StripeController.java
@@ -1,12 +1,15 @@
 package org.success.payment.stripe.controllers;
 
+import com.stripe.model.Event;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.success.payment.stripe.services.StripeCheckout;
+import org.success.payment.stripe.services.StripeWebhooks;
 
 @RestController
 @RequestMapping("/stripe")
@@ -15,10 +18,23 @@ public class StripeController {
     @Autowired
     private StripeCheckout stripeCheckout;
 
+    @Autowired
+    private StripeWebhooks stripeWebhooks;
+
     @PostMapping("/create-checkout-session")
     public ResponseEntity createCheckout(){
         try{
             return ResponseEntity.ok().body(stripeCheckout.createCheckoutSession());
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(e.getLocalizedMessage());
+        }
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity webhook(@RequestBody Event event){
+        try{
+            stripeWebhooks.processWebhook(event);
+            return ResponseEntity.ok().build();
         }catch (Exception e){
             return ResponseEntity.status(500).body(e.getLocalizedMessage());
         }
--- backend/src/main/java/org/success/payment/stripe/services/StripeWebhooks.java
+++ backend/src/main/java/org/success/payment/stripe/services/StripeWebhooks.java
@@ -1,20 +1,34 @@
 package org.success.payment.stripe.services;
 
+import com.stripe.Stripe;
 import com.stripe.model.Charge;
 import com.stripe.model.Event;
 import com.stripe.model.EventDataObjectDeserializer;
 import com.stripe.model.StripeObject;
+import com.stripe.model.checkout.Session;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.success.payment.stripe.StripeContext;
 
 @Service
 public class StripeWebhooks extends StripeContext {
 
+    @Async
     public void processWebhook(Event event){
-        if(event.getType().equals("charge.succeeded")){
+        try{
+            event.setApiVersion(Stripe.API_VERSION);
             StripeObject object = this.getAbstractStripeObject(event);
-            Charge charge = (Charge) object;
-            System.out.println("charge");
+            if(event.getType().equals("checkout.session.completed")){//cobrança realizada com sucesso
+                Session checkout = (Session) object;
+                System.out.println(checkout);
+            }
+            if(event.getType().equals("charge.succeeded")){//cobrança realizada com sucesso
+                Charge charge = (Charge) object;
+                System.out.println(charge);
+            }
+        }catch (Exception e){
+            System.out.println("ERRO AO PROCESSAR WEBHOOK");
+            System.out.println(e.getLocalizedMessage());
         }
     }
 
