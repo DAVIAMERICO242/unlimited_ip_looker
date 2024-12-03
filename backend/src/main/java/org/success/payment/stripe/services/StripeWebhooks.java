d793784de1081c7a13870b6c7fd444c76cc629b4
Your Name <you@example.com>
2024-12-02 23:53:44 -0300

    OI

--- backend/src/main/java/org/success/payment/stripe/services/StripeWebhooks.java
+++ backend/src/main/java/org/success/payment/stripe/services/StripeWebhooks.java
@@ -1,8 +1,25 @@
 package org.success.payment.stripe.services;
 
+import com.stripe.model.Charge;
+import com.stripe.model.Event;
+import com.stripe.model.EventDataObjectDeserializer;
+import com.stripe.model.StripeObject;
 import org.springframework.stereotype.Service;
 import org.success.payment.stripe.StripeContext;
 
 @Service
 public class StripeWebhooks extends StripeContext {
+
+    public void processWebhook(Event event){
+        if(event.getType().equals("charge.succeeded")){
+            StripeObject object = this.getAbstractStripeObject(event);
+            Charge charge = (Charge) object;
+            System.out.println("charge");
+        }
+    }
+
+    private StripeObject getAbstractStripeObject(Event event){
+        EventDataObjectDeserializer dataObjectDeserializer = event.getDataObjectDeserializer();
+        return dataObjectDeserializer.getObject().get();
+    }
 }
