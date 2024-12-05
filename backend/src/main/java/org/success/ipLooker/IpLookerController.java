ff01add07689dffdf0957c7f29b784c95c2e0494
Your Name <you@example.com>
2024-12-05 01:06:16 -0300

    OI

--- backend/src/main/java/org/success/ipLooker/IpLookerController.java
+++ backend/src/main/java/org/success/ipLooker/IpLookerController.java
@@ -1,5 +1,7 @@
 package org.success.ipLooker;
 
+import com.ip2location.IP2Location;
+import com.ip2location.IPResult;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
@@ -11,17 +13,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.success.Main;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @RestController
 @RequestMapping("/look_ip")
 public class IpLookerController {
 
+    private Path file;
+
     private DatabaseReader reader = new DatabaseReader.Builder(Main.class.getClassLoader().getResourceAsStream("GeoLite2-City.mmdb")).build();
 
     public IpLookerController() throws IOException {
-
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("IP2LOCATION-LITE-DB11.IPV6.BIN");
+        file = Files.createTempFile(null, ".BIN");
+        Files.copy(inputStream, file, StandardCopyOption.REPLACE_EXISTING);
     }
 
     @GetMapping
@@ -51,6 +64,32 @@ public class IpLookerController {
                             response.getPostal().getCode(),
                             response.getLocation().getLatitude(),
                             response.getLocation().getLongitude())
+            );
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(e.getLocalizedMessage());
+        }
+
+    }
+
+    @GetMapping("/v2")
+    public ResponseEntity getLocation2(@RequestParam(required = false) String ip, Boolean from_me, HttpServletRequest request) throws IOException, GeoIp2Exception {
+        try{
+            IP2Location builder = new IP2Location();
+            builder.Open(Paths.get(file.toUri()).toString());
+            IPResult result = builder.IPQuery("2804:3230:80:3b00:9c1d:d306:5440:4f9c");
+            builder.Close();
+            return ResponseEntity.ok().body(
+                    new LocationResponse(
+                            "2804:3230:80:3b00:9c1d:d306:5440:4f9c",
+                    null,
+                            result.getCountryLong(),
+                            result.getRegion(),
+                            result.getCity(),
+                            result.getTimeZone(),
+                            result.getZipCode(),
+                            (double) result.getLatitude(),
+                            (double) result.getLongitude()
+                    )
             );
         }catch (Exception e){
             return ResponseEntity.status(500).body(e.getLocalizedMessage());
