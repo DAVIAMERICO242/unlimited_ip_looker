93bb0d425f043f5d293d8d70923aff3889ed82ff
INTELIGENCIA <darodriguesufc@gmail.com>
2024-12-09 13:11:42 -0300

    OI

--- backend/src/main/java/org/success/customer/repositories/CustomerRepository.java
+++ backend/src/main/java/org/success/customer/repositories/CustomerRepository.java
@@ -2,7 +2,17 @@ package org.success.customer.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.success.customer.entities.Customer;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer,String>, JpaSpecificationExecutor<Customer> {
+
+    @Query("SELECT c FROM Customer c WHERE c.stripeCustomer.stripeId =: stripeId")
+    Optional<Customer> findByStripeId(String stripeId);
+
+    Optional<Customer> findByEmail(String email);
+
+
 }
--- backend/src/main/java/org/success/payment/processor/SubscriptionProcessor.java
+++ backend/src/main/java/org/success/payment/processor/SubscriptionProcessor.java
@@ -12,31 +12,46 @@ import org.success.user.DTOs.CreatedUserWithRandomPass;
 import org.success.user.services.UserService;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class SubscriptionProcessor extends SubscriptionContext {
 
     @Transactional
-    public void processStripeAfterCheckout(String stripeCustomerId, String name,String email,String phone){
+    public void processStripeAfterCheckout(String stripeCustomerId,String name,String email,String phone){//como eu posso gerar o checkout a partir do stripe customerId nao é um problema
         try{
-            CreatedUserWithRandomPass output = userService.createUserWithRandomPass(email);
-            Customer customer = new Customer();
-            customer.setUser(output.createdUser());
-            customer.setName(name);
-            customer.setEmail(email);
-            customer.setPhone(phone);
-            customer.setToken(ipLookerKeyService.generateRandomKey());
-            customer.setTokenActive(true);
-            customer.setLastPayment(LocalDateTime.now());
-            Customer savedCustomer = customerRepository.save(customer);
-            StripeCustomer stripeCustomer = new StripeCustomer();
-            stripeCustomer.setStripeId(stripeCustomerId);
-            stripeCustomer.setSystemCustomer(savedCustomer);
-            stripeCustomerRepository.save(stripeCustomer);
+            Optional<Customer> customerOPT = customerRepository.findByStripeId(stripeCustomerId);
+            if(customerOPT.isEmpty()){//ativar plano primeira vez
+                this.activePlanFirstTime(stripeCustomerId,name,email,phone);
+            }else{
+                this.reactivePlan(customerOPT.get());
+            }
         }catch (Exception e){
             System.out.println("ERRO AO SALVAR CUSTOMER STRIPE");
         }
+    }
 
+    private void activePlanFirstTime(String stripeCustomerId,String name,String email,String phone){
+        CreatedUserWithRandomPass output = userService.createUserWithRandomPass(email);
+        Customer customer = new Customer();
+        customer.setUser(output.createdUser());
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setPhone(phone);
+        customer.setToken(ipLookerKeyService.generateRandomKey());
+        customer.setTokenActive(true);
+        customer.setLastPayment(LocalDateTime.now());
+        Customer savedCustomer = customerRepository.save(customer);
+        StripeCustomer stripeCustomer = new StripeCustomer();
+        stripeCustomer.setStripeId(stripeCustomerId);
+        stripeCustomer.setSystemCustomer(savedCustomer);
+        stripeCustomerRepository.save(stripeCustomer);
+    }
+
+    private void reactivePlan(Customer customer){
+        customer.setTokenActive(true);
+        customer.setLastPayment(LocalDateTime.now());
+        Customer savedCustomer = customerRepository.save(customer);
     }
 
 
